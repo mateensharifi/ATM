cb512acdcbeb99ab0a9bd88e2f3e82c81553a660
mateensharifi <mateensharifi11@gmail.com>
2021-08-31 13:40:03 -0700

    Rewrote constructor to randomize ID

--- src/ATM.java
+++ src/ATM.java
@@ -1,11 +1,21 @@
+import java.util.HashMap;
+import java.util.Random;
 
 public class ATM {
 	private String iD;
-	private double balance;
+	private double balance = 0;
 	
-	public ATM (String iD, double balance) {
-		this.iD = iD;
-		this.balance = balance;
+	public ATM () {
+		char[] code = new char[10];
+		for (int i = 0; i < 10; i++) {
+			Random r = new Random();
+			code[i] = (char)(r.nextInt(26) + 'a');
+		}
+		StringBuilder toRet = new StringBuilder();
+		for (int i = 0; i < 10; i++) {
+			toRet.append(code[i]);
+		}
+		iD = toRet.toString();
 	}
 	
 	public double deposit (String inputID, double depositAmount) {
--- src/ATMTester.java
+++ src/ATMTester.java
@@ -1,9 +1,9 @@
 
 public class ATMTester {
 	public static void main (String [] args) {
-		ATM test = new ATM ("123", 5);
+		ATM test = new ATM ();
 		System.out.println(test.getID());
-		System.out.println(test.deposit("12", 5));
+		System.out.println(test.deposit("1243235", 5));
 		System.out.println(test.getID());
 	}
 }
