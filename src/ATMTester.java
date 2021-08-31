3d0793fa0cf2f19be1f1b724228754a95a7fd603
mateensharifi <mateensharifi11@gmail.com>
2021-08-30 19:46:38 -0700

    Tested deposit

--- src/ATMTester.java
+++ src/ATMTester.java
@@ -3,7 +3,7 @@ public class ATMTester {
 	public static void main (String [] args) {
 		ATM test = new ATM ("123", 5);
 		System.out.println(test.getID());
-		System.out.println(test.deposit("123", 5));
+		System.out.println(test.deposit("12", 5));
 		System.out.println(test.getID());
 	}
 }
