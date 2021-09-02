b64c410896ec8bf34037c99fc62c3da708bdf627
mateensharifi <mateensharifi11@gmail.com>
2021-09-02 14:14:29 -0700

    Updated tester

--- src/ATMTester.java
+++ src/ATMTester.java
@@ -1,7 +1,7 @@
 
 public class ATMTester {
 	public static void main (String [] args) throws Exception {
-		
+
 		ATM test = new ATM ();
 		/**
 		System.out.println(test.getID());
@@ -9,14 +9,14 @@ public class ATMTester {
 		System.out.println(test.getID());
 		System.out.println(test.deposit("4", 12));
 		**/
-		
+
 		test.deposit("Winfrey", 25159684372.43);
 		test.deposit("Bezos", 193495330293.43);
 		test.deposit("Bezos", 20394353.35);
-		test.withdraw("Bezos", 3593293.66);;
+		test.withdraw("Bezos", 3593293.66);
 		test.deposit("MrTheiss",64033.55);
-		test.withdraw("MrTheiss", 443964.46);
-		
+		//test.withdraw("MrTheiss", 443964.46);
+
 		System.out.println(test.checkBalance("Bezos"));
 		System.out.println(test.checkBalance("Winfrey"));
 		System.out.println(test.checkBalance("YoMama"));
