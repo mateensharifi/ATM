d79ce4b7b740ad39b938413b7b9f0517a21031c1
mateensharifi <mateensharifi11@gmail.com>
2021-09-02 14:12:56 -0700

    Updated tester

--- src/ATMTester.java
+++ src/ATMTester.java
@@ -13,9 +13,9 @@ public class ATMTester {
 		test.deposit("Winfrey", 25159684372.43);
 		test.deposit("Bezos", 193495330293.43);
 		test.deposit("Bezos", 20394353.35);
-		test.withdraw("Bezos", 3593293.66);
+		test.withdraw("Bezos", 3593293.66);;
 		test.deposit("MrTheiss",64033.55);
-		//test.withdraw("MrTheiss", 443964.46);
+		test.withdraw("MrTheiss", 443964.46);
 		
 		System.out.println(test.checkBalance("Bezos"));
 		System.out.println(test.checkBalance("Winfrey"));
